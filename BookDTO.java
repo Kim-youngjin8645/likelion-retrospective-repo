7e9c3a1e7ea3f47bfa48a0f924575eb79c05fba9
wlswls <rla8645@hufs.ac.kr>
2025-09-16 23:01:05 +0900

    0916Lib실습미완성

---
+++ BookDTO.java
@@ -0,0 +1,54 @@
+package LibraryPractice;
+
+public class BookDTO {
+    private long id;
+    private String author;
+    private String title;
+    private boolean isRented;
+
+    //생성자.
+    public BookDTO(long id, String author, String title, boolean isRented) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.isRented = isRented;//대여가능
+    }
+
+    //getter,setter
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isRented() {
+        return isRented;
+    }
+    public void setRented(boolean rented) {
+        isRented = rented;
+    }
+
+
+    //toString재정의.
+    @Override
+    public String toString() {
+        String status= isRented ? "대출중" : "대여가능";
+        return "책번호: "+id+"번 저자: "+author+"의 "+title+"은 "+status;
+    }
+
+}
